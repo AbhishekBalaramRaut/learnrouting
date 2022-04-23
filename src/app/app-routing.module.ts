2dc95cc3e1e8405a3ddc38d51cf45c240edd9173
AbhishekBalaramRaut <41266973+AbhishekBalaramRaut@users.noreply.github.com>
2022-04-23 17:31:15 +0530

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { HomeComponent } from './first/home/home.component';
+import { MessagesComponent } from './first/messages/messages.component';
+import { ProfileComponent } from './first/profile/profile.component';
+import { SettingsComponent } from './first/settings/settings.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'first', component: FirstComponent ,
+    children: [
+      {path: 'home', component: HomeComponent},
+      {path: 'profile', component: ProfileComponent},
+      {path: 'settings', component: SettingsComponent},
+      {path: 'messages', component: MessagesComponent},
+      { path: '', redirectTo: 'home', pathMatch: 'full' }
+    ]},
+  { path: 'second', component: SecondComponent },
+  { path: 'third', component: ThirdComponent},
+  { path: '', redirectTo: 'first', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { HomeComponent } from './first/home/home.component';
+import { ProfileComponent } from './first/profile/profile.component';
+import { MessagesComponent } from './first/messages/messages.component';
+import { SettingsComponent } from './first/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    HomeComponent,
+    ProfileComponent,
+    MessagesComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
